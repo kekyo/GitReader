5c71b997c7d1aa3f523216704fbfe6f498729566
Kouji Matsui <k@kekyo.net>
2023-04-22 08:29:20 +0900

    Splitted repository factory implementations.

---
+++ GitReader/Primitive/RepositoryFactoryExtension.cs
@@ -0,0 +1,37 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// GitReader - Lightweight Git local repository traversal library.
+// Copyright (c) Kouji Matsui (@kozy_kekyo, @kekyo@mastodon.cloud)
+//
+// Licensed under Apache-v2: https://opensource.org/licenses/Apache-2.0
+//
+////////////////////////////////////////////////////////////////////////////
+
+using GitReader.Internal;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GitReader.Primitive;
+
+public static class RepositoryFactoryExtension
+{
+    public static async Task<Repository> OpenAsync(
+        this RepositoryFactory _,
+        string path, CancellationToken ct = default, bool forceUnlock = false)
+    {
+        var repositoryPath = Path.GetFileName(path) != ".git" ?
+            Utilities.Combine(path, ".git") : path;
+
+        if (!Directory.Exists(repositoryPath))
+        {
+            throw new ArgumentException("Repository does not exist.");
+        }
+
+        var lockPath = Utilities.Combine(repositoryPath, "index.lock");
+        var locker = await TemporaryFile.CreateLockFileAsync(lockPath, ct, forceUnlock);
+
+        return new(repositoryPath, locker);
+    }
+}
