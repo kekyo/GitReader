52030585c72e9261101e39fc88d96e5f2994e85e
Kouji Matsui <k@kekyo.net>
2023-04-27 17:56:32 +0900

    Added test3.

--- samples/test-large-repo.sh
+++ samples/test-large-repo.sh
@@ -28,3 +28,17 @@ diff -u gitlog2.txt output.txt
 cd ..
 
 echo "Done."
+
+echo "---------------------------------------"
+echo "Test git..."
+
+rm -rf git
+git clone https://github.com/git/git.git
+cd git
+git log --abbrev-commit --abbrev=40 --decorate --all > gitlog1.txt
+mono ../gitlogtest/bin/Debug/net48/gitlogtest.exe --fix gitlog1.txt > gitlog2.txt
+mono ../gitlogtest/bin/Debug/net48/gitlogtest.exe --log . > output.txt
+diff -u gitlog2.txt output.txt
+cd ..
+
+echo "Done."
