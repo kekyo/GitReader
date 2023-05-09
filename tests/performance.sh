849fac0ebe925ce9cfca13501c79fd4b197b68ea
Kouji Matsui <k@kekyo.net>
2023-05-09 18:48:30 +0900

    Added performance test script.

---
+++ tests/performance.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -rf git
+git clone https://github.com/git/git.git
+
+cd git
+
+echo "Start..."
+
+start_time=`date +%s`
+
+dotnet ../gitlogtest/bin/Release/net6.0/gitlogtest.dll . > output.txt
+
+end_time=`date +%s`
+ 
+echo "Finished."
+
+run_time=$((end_time - start_time))
+echo "Time:" $run_time
+
+cd ..
+
+echo "Done."
+echo ""
