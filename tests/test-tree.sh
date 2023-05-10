6c7eaba646ab65c76ea2145100fbb72b91355e9e
Kouji Matsui <k@kekyo.net>
2023-05-10 11:20:17 +0900

    Added bash packaging script.

--- tests/test-tree.sh
+++ tests/test-tree.sh
@@ -3,15 +3,22 @@
 rm -rf git
 git clone https://github.com/git/git.git
 
+echo "Step 1: checkout with git command."
+
 cd git
 git checkout 5597cfdf47db94825213fefe78c4485e6a5702d8
 git checkout-index -a -f --prefix=../export.orig/
 cd ..
 
+echo "Step 2: extract with gitexporttest."
+
 rm -rf export
+mkdir export
 cd export
 dotnet ../gitexporttest/bin/Release/net6.0/gitexporttest.dll ../git 5597cfdf47db94825213fefe78c4485e6a5702d8
 cd ..
+
+echo "Step 3: Makes differ."
 
 diff -r -u export.orig/ export/
 
